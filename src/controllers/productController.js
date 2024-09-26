5a1f086f41f5d9052d3a8dc72de1f3a35d41ae63
anaaclarasilverio <ds.46277728822@gmail.com>
2024-09-26 08:24:42 -0300

    Criação de metodo de delete api

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import { getAllProducts, createProduct, getProducts, getProductByld } from "../services/productService.js";
+import { getAllProducts, createProduct, getProductByld, updateProduct, deleteProduct } from "../services/productService.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -12,27 +12,47 @@ export const getProducts = async (req, res) => {
     }
 };
 
-export const createProductController = async(req, res) => {
-    try{
-        const newProduct = await createProduct(req, body);
+export const createProductController = async (req, res) => {
+    try {
+        const newProduct = await createProduct(req.body);
         res.status(201).json(newProduct)
     }
-    catch(error){
-        res.status(500).json({error: "erro ao criar produto"})
+    catch (error) {
+        res.status(500).json({ error: "erro ao criar produto" })
     }
 }
 
-export const getProduct = async(req, res) =>{
-    try{
+export const getProduct = async (req, res) => {
+    try {
         const product = await getProductByld(req.params.id)
-        if(product){
-
-        }else{4
-            res.status(500).json({error: "Produto não encontrado"})
-
+        if (product) {
+            res.json(product)
+        } else {
+            res.status(500).json({ error: "Produto não encontrado" })
         }
     }
-    catch (error){
-        res.status(500).json({erro: "Erro ao buscar produtos"});
+    catch (error) {
+        res.status(500).json({ erro: "Erro ao buscar produtos" });
     }
+};
+
+export const updateProductController = async (req, res) => {
+    try {
+        const product = await updateProduct(req.params.id, req.body);
+        res.json(product);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Erro ao atualizar produto" });
+    }
+}
+
+export const deleteProductController = async (req, res) => {
+    try {
+        await deleteProduct(req.params.id);
+        res.status(204).send();
+    }
+    catch (error) {
+        res.status(500).json({ error: "Erro ao deletar produto" });
+    }
+
 };
--- src/services/productService.js
+++ src/services/productService.js
@@ -6,7 +6,7 @@ export const getAllProducts = async () => {
     return await prisma.product.findMany();
 }
 
-export const createProduct = async () => {
+export const createProduct = async (productData) => {
     return await prisma.product.create({
         data: productData
 
@@ -17,4 +17,17 @@ export const getProductByld = async (id) => {
     return await prisma.product.findUnique({
         where:{id:parseInt(id)},
     });
+};
+
+export const updateProduct = async(id, productData) => {
+    return await prisma.product.update({
+        where:{id:parseInt(id)},
+        data:productData,
+    })
+};
+
+export const deleteProduct = async(id) => {
+    return await prisma.product.delete({
+        where:{id:parseInt(id)},
+    })
 }
